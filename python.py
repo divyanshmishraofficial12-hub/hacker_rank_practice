2196ea24bdd64b5476463a4841a8f200226a7d29
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-30 13:40:05 +0530

    Add odd/even and weirdness checker in python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+#   ques 1
+no=int(input("entr the number"))
+
+if no % 2!=0:
+    print("odd")
+elif no >=2 and no <=5:
+    print("not weird..!")
+elif no >=6 and no <= 20:
+    print("more weird..!")
+else:
+    print("more and more weird..!")
